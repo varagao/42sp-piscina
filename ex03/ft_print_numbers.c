7543c280d50091df4ac0f4179bfd71044aa15dcb
varagao <oficina.zzb@gmail.com>
2022-07-20 11:42:41 -0300

    c00 ex03 v1.1 -- numeros como char

--- ex03/ft_print_numbers.c
+++ ex03/ft_print_numbers.c
@@ -1,19 +1,14 @@
-
 #include <unistd.h>
 
-void	ft_print_alphabet(void)
+void	ft_print_numbers(void)
 {
 	char	c;
 
 	c = '0';
+
 	while (c <= '9')
 	{
 		write(1, &c, 1);
 		c++;
 	}
-}
-
-int main(void)
-{
-    ft_print_alphabet();
 }
